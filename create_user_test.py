457fa83b507dc4a79c9d30840ef5ae4fccf2c1f8
Korrig <KryukovKorrig@yandex.ru>
2023-05-02 18:14:32 +0300

    My first commit

---
+++ create_user_test.py
@@ -0,0 +1,11 @@
+import sender_stand_request
+from data import headers, user_body
+
+def test_create_user_2_letter_in_first_name_get_success_response():
+    user_response = sender_stand_request.post_new_user(user_body)
+    assert user_response.status_code == 201
+    assert user_response.json()["authToken"] != ""
+    users_table_response = sender_stand_request.get_users_table()
+    str_user = user_body["firstName"] + "," + user_body["phone"] + "," \
+               + user_body["address"] + ",,," + user_response.json()["authToken"]
+    assert users_table_response.text.count(str_user) == 1
---
+++ data.py
@@ -0,0 +1,9 @@
+headers = {
+    "Content-Type": "application/json"
+}
+
+user_body = {
+    "firstName": "Аа",
+    "phone": "+79995553322",
+    "address": "г. Москва, ул. Пушкина, д. 10"
+}
